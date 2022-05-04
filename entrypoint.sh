2e67aeee17b983cd67a5678257b60abfe7f6db45
Tomash Gombosh <blackjneco@gmail.com>
2022-05-04 07:23:12 +0300

    [INFRA] Update the google chrome installation and add clean up (#4)
    * [INFRA] Update the chrome installation
    
    * [INFRA] Update the chrome installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "HUB_URL -> ${HUB_URL}"
 echo "SELENIUM_VERSION -> ${SELENIUM_VERSION}"
 echo "SESSION -> ${SESSION}"
 
-java -jar selenium-server-${SELENIUM_VERSION}.jar node --hub ${HUB_URL} --config config.toml 
+java -jar selenium-server-${SELENIUM_VERSION}.jar node --hub ${HUB_URL} --config config.toml
