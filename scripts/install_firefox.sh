f2a90e2589926ee3801ab97e1deaffd1824e09ee
TomashGombosh <blackjneco@gmail.com>
2022-05-05 21:38:44 +0300

    [INFRA] Change the geckodriver location

--- scripts/install_firefox.sh
+++ scripts/install_firefox.sh
@@ -9,7 +9,7 @@ echo "Firefox install successfully"
 
 wget https://github.com/mozilla/geckodriver/releases/download/v${GECKO_DRIVER_VERSION}/geckodriver-v${GECKO_DRIVER_VERSION}-linux64.tar.gz
 tar -xvf geckodriver-v${GECKO_DRIVER_VERSION}-linux64.tar.gz
-mv geckodriver /usr/local/bin/
+mv geckodriver /usr/bin/
 chown root:root /usr/bin/geckodriver
 chmod +x /usr/bin/geckodriver
 echo "Geckodriver install successfully"
