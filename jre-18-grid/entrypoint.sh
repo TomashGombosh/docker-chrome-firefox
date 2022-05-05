6f6e099f856b7b54c989abc2fea627776b17c0b2
TomashGombosh <blackjneco@gmail.com>
2022-05-05 21:21:15 +0300

    [JRE-18][JRE-17] Update the entrypoint.sh script

---
+++ jre-18-grid/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "HUB_URL -> ${HUB_URL}"
+echo "SELENIUM_VERSION -> ${SELENIUM_VERSION}"
+echo "SESSION -> ${SESSION}"
+
+java -jar selenium-server-${SELENIUM_VERSION}.jar node --hub ${HUB_URL} --config config.toml
