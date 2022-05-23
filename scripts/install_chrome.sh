b09725e71b0939201233962bbd8e7c06f081050d
TomashGombosh <blackjneco@gmail.com>
2022-05-23 22:26:50 +0300

    [LINT] Remove typo

--- scripts/install_chrome.sh
+++ scripts/install_chrome.sh
@@ -7,7 +7,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 echo "Chrome install successfully"
 
-wget https://chromedriver.storage.googleapis.com/101.0.4951.41/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 mv chromedriver /usr/bin/
 chown root:root /usr/bin/chromedriver
