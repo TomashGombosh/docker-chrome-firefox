3ceff6d82dc68b9d533b0c136feccc6a8ca1a69d
TomashGombosh <blackjneco@gmail.com>
2022-05-30 22:43:23 +0300

    [INFRA] Update node

--- node-16/scripts/install_nodejs.sh
+++ node-16/scripts/install_nodejs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
-deb https://deb.nodesource.com/node_14.x focal main
-deb-src https://deb.nodesource.com/node_14.x focal main
+deb https://deb.nodesource.com/node_16.x focal main
+deb-src https://deb.nodesource.com/node_16.x focal main
 apt-get -y install nodejs
