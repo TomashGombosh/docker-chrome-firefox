c82e996f8a6f8fc0d366fc92aecf840484b67a2e
Tomash Gombosh <blackjneco@gmail.com>
2022-06-03 16:26:00 +0300

    [INFRA] Update the Selenium grid container and add a windows support (#18)
    * [INFRA] Update the Selenium grid container and add a windows support
    
    * [INFRA] Update the version
    
    * [INFRA] Add message to the else block
    
    * [INFRA] Update the JRE-11 and JRE-17 version

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$param1=$args[0]
+$FOLDER=$args[0]
 $PATH = Get-Location;
-mkdir -p "$PATH/$param1/scripts/"
-Get-ChildItem -Path "./scripts\*.sh" -Recurse | Copy-Item -Destination "$PATH/jre-11/scripts/"
+New-Item -ItemType Directory -Force -Path "$PATH/$FOLDER/scripts/"
+Get-ChildItem -Path "./scripts\*.sh" -Recurse | Copy-Item -Destination "$PATH/$FOLDER/scripts/"
--- jre-17-grid/entrypoint.sh
+++ jre-17-grid/entrypoint.sh
@@ -3,14 +3,17 @@
 echo "HUB_URL -> ${HUB_URL}"
 echo "SELENIUM_VERSION -> ${SELENIUM_VERSION}"
 echo "TIMEOUT -> ${TIMEOUT}"
+echo "NODE_IP Address -> ${NODE_IP}"
 
-if [[ $TIMEOUT == "" ]];
-then
-    echo "Without timeout"
+if [[ $TIMEOUT == "" && $NODE_IP == "" ]] ;then
+    echo "Without timeout and with node host by default"
     java -jar selenium-server-${SELENIUM_VERSION}.jar node --hub ${HUB_URL}
+elif [[ $NODE_IP != "" ]] ;then
+    echo "Without timeout and with node host from env value"
+    java -jar selenium-server-${SELENIUM_VERSION}.jar node --hub ${HUB_URL} --bind-host false --host ${NODE_IP}
 else
     echo "With timeout"
-    java -jar selenium-server-${SELENIUM_VERSION}.jar node --hub ${HUB_URL} &
+    java -jar selenium-server-${SELENIUM_VERSION}.jar node --hub ${HUB_URL} --bind-host false --host ${NODE_IP} &
     sleep $TIMEOUT
     kill "$!"
 fi
