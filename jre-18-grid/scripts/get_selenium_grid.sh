aed8cfcfc14db52fe99ca4ab308ee5d993b61fc6
TomashGombosh <blackjneco@gmail.com>
2022-06-10 23:44:11 +0300

    [INFRA] Version update using cirle ci

--- jre-18-grid/scripts/get_selenium_grid.sh
+++ jre-18-grid/scripts/get_selenium_grid.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SELENIUM_VERSION=$1
-wget https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.1.0/selenium-server-$SELENIUM_VERSION.jar
+SELENIUM_DOWNLOAD_VERSION=$(echo $SELENIUM_VERSION | cut -d'.' -f 1-2)
+wget https://github.com/SeleniumHQ/selenium/releases/download/selenium-$SELENIUM_DOWNLOAD_VERSION.0/selenium-server-$SELENIUM_VERSION.jar
 chmod +x selenium-server-$SELENIUM_VERSION.jar
