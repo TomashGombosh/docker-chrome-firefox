7974c71f60bf99de61d5b49042b0c74b82759303
TomashGombosh <blackjneco@gmail.com>
2022-06-30 08:47:21 +0300

    [INFRA] Update the paralelism

--- tests_handler.sh
+++ tests_handler.sh
@@ -3,7 +3,7 @@
 cd $TEST_FOLDER
 docker build --build-arg CHROMEDRIVER_VERSION=$CHROMEDRIVER_VERSION \
     --build-arg GECKODRIVER_VERSION=$GECKODRIVER_VERSION \
-    --build-arg GECKODRIVER_VERSION=$GRID_VERSION \
+    --build-arg GRID_VERSION=$GRID_VERSION \
     -t chrome-firefox:local .
 cd ..
 cd test
