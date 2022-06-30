771d39fa0a4301438e89dfed5d473bf479c2ede8
TomashGombosh <blackjneco@gmail.com>
2022-06-30 08:11:27 +0300

    [QA] Update test script

--- tests_handler.sh
+++ tests_handler.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 cd $TEST_FOLDER
-docker build --build-arg CHROMEDRIVER_VERSION=$CHROMEDRIVER_VERSION \
-            --build-arg GECKODRIVER_VERSION=$GECKODRIVER_VERSION \
-            -t chrome-firefox:latest .
+docker build --build-arg CHROMEDRIVER_VERSION=$CHROMEDRIVER_VERSION --build-arg GECKODRIVER_VERSION=$GECKODRIVER_VERSION -t chrome-firefox:local .
 cd ..
 cd test
 docker build --build-arg JAVA_VERSION=$JAVA_TEST_VERSION --build-arg GECKODRIVER_VERSION=$GECKODRIVER_VERSION --build-arg CHROMEDRIVER_VERSION=$CHROMEDRIVER_VERSION -t test:local .   
