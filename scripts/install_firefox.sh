a6e79f57d010cfa57323b28602623643b8c6eccf
TomashGombosh <blackjneco@gmail.com>
2022-12-21 11:25:21 +0200

    [FIREFOX] Snap install firefox

--- scripts/install_firefox.sh
+++ scripts/install_firefox.sh
@@ -4,10 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 GECKO_DRIVER_VERSION=$1
 
 apt-get update
-wget -O ~/Firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64"
-tar xjf ~/Firefox.tar.bz2 -C /opt/
-mv /usr/lib/firefox/firefox /usr/lib/firefox/firefox_backup
-ln -s /opt/firefox/firefox /usr/bin/
+apt-get install -y firefox
 echo "Firefox install successfully"
 
 wget https://github.com/mozilla/geckodriver/releases/download/v${GECKO_DRIVER_VERSION}/geckodriver-v${GECKO_DRIVER_VERSION}-linux64.tar.gz
