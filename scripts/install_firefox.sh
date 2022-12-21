e9a98d093c6c483d4e74a5be1515abc6045fb08a
TomashGombosh <blackjneco@gmail.com>
2022-12-21 11:33:30 +0200

    [FIREFOX] Add manual install of the firefox

--- scripts/install_firefox.sh
+++ scripts/install_firefox.sh
@@ -3,14 +3,10 @@ export DEBIAN_FRONTEND=noninteractive
 
 GECKO_DRIVER_VERSION=$1
 
-apt-get update
-add-apt-repository ppa:mozillateam/ppa
-echo 'Package: *Pin: release o=LP-PPA-mozillateamPin-Priority: 1001' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
-apt-get install -y firefox
 snap install firefox
 echo "Firefox install successfully"
 
+apt-get update
 wget https://github.com/mozilla/geckodriver/releases/download/v${GECKO_DRIVER_VERSION}/geckodriver-v${GECKO_DRIVER_VERSION}-linux64.tar.gz
 tar -xvf geckodriver-v${GECKO_DRIVER_VERSION}-linux64.tar.gz
 mv geckodriver /usr/bin/
