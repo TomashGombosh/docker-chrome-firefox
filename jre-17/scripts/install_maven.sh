29d78d41a91f9ee4f9ce2facdc8932f4984c04c1
TomashGombosh <blackjneco@gmail.com>
2023-01-26 22:55:18 +0200

    [FIX] Bug with maven

---
+++ jre-17/scripts/install_maven.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+wget https://dlcdn.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
+tar xvf apache-maven-${MAVEN_VERSION}-bin.tar.gz
+cp -r apache-maven-${MAVEN_VERSION}/* /usr/share/maven
