0580ae88aeecfa2a009978246a6ce51613990ec9
TomashGombosh <blackjneco@gmail.com>
2023-01-27 02:14:13 +0200

    [INFRA] Fix tests

--- tests_handler.sh
+++ tests_handler.sh
@@ -4,6 +4,7 @@ cd $TEST_FOLDER
 docker build --build-arg CHROMEDRIVER_VERSION=$CHROMEDRIVER_VERSION \
     --build-arg GECKODRIVER_VERSION=$GECKODRIVER_VERSION \
     --build-arg SELENIUM_GRID_VERSION=$GRID_VERSION \
+    --build-arg MAVEN_VERSION=$MAVEN_VERSION \
     -t chrome-firefox:local .
 cd ..
 cd test
