74300ca16eebcf5c2a73895b0d344aca0aa46cac
TomashGombosh <blackjneco@gmail.com>
2023-01-27 02:03:35 +0200

    [FIX] Maven installation

--- jre-18/scripts/install_maven.sh
+++ jre-18/scripts/install_maven.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
+MAVEN_VERSION=$1
 
+apt-get install -y maven
+rm -r /usr/share/maven
 wget https://dlcdn.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
 tar xvf apache-maven-${MAVEN_VERSION}-bin.tar.gz
 mkdir /usr/share/maven
-cp -r apache-maven-${MAVEN_VERSION}/* /usr/share/maven
+cp -rf apache-maven-${MAVEN_VERSION}/* /usr/share/maven
+mvn -version
+rm -r apache-maven-${MAVEN_VERSION}-bin.tar.gz
